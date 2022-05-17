8fd3f20698c01544bcfdc9c4d96e3db8bc355a57
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-17 13:22:30 +0300

    routes updates

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-
-
 @app.route('/')
+@app.route('/home')
+@app.route('/Blogpost')
 def Welcome():
-    return "My BlogPOST"
-
-
+    return "Blogpost"
 
 if __name__ == "__main__":
     app.run(debug=True)
